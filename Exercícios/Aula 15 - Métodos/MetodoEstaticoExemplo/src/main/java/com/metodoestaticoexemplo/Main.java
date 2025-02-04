d80ad8c0e0c4cfef8e2d93c37c343c9d84994936
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-02-04 17:13:25 -0300

    Inclusão de novos métodos no exercício 02 da aula de métodos

--- Exercícios/Aula 15 - Métodos/MetodoEstaticoExemplo/src/main/java/com/metodoestaticoexemplo/Main.java
+++ Exercícios/Aula 15 - Métodos/MetodoEstaticoExemplo/src/main/java/com/metodoestaticoexemplo/Main.java
@@ -3,13 +3,36 @@ package com.metodoestaticoexemplo;
 import java.util.Scanner;
 
 public class Main {
-    // Método estático (pertencente à própria classe)
+    // Métodos estático (pertencentes à própria classe)
+    // soma
+    /*
+    public static double somar(double a, double b){
+        return a + b; // soma a e b
+    }
+    */
+
+    // subtração
+    /*
+    public static double subtrair(double a, double b){
+        return a - b; // subtrai b de a
+    }
+    */
+
+    // multiplicação
+    /*
+    public static double multiplicar(double a, double b){
+        return a * b; // multiplica a por b
+    }
+     */
+
+    // divisão
     public static double dividir(double a, double b){
         if (b == 0) {
             return a; // se b for 0, retorna a (alternativa para a impossibilidade da divisão por 0)
         }
         return a / b; // divide a por b
     }
+
 
     public static void main(String[] args) {
         // Criando um objeto para fazer leitura de dados
@@ -25,7 +48,13 @@ public class Main {
 
         System.out.println("\nOs números informados foram: " + n1 + " e " + n2);
 
-        // Calculando a divisão com o método estático criado e exibindo o resultado pro usuário
+        // Calculando as operações com os métodos estáticos criados e exibindo o resultado na tela
+        /*
+        System.out.printf("O resultado da soma dos números informados é %.1f\n", Main.somar(n1, n2));
+        System.out.printf("O resultado da subtração dos números informados é %.1f\n", Main.subtrair(n1, n2));
+        System.out.printf("O resultado da multiplicação dos números informados é %.1f\n", Main.multiplicar(n1, n2));
+        */
         System.out.printf("O resultado da divisão dos números informados é %.1f", Main.dividir(n1, n2));
+
     }
 }
